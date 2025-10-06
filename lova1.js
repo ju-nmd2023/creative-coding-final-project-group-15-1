eb2450915606e37d55fb0fbe4e9b12745cc78e21
filippasofia <fufi23pw@student.ju.se>
2025-10-06 14:57:39 +0200

    made flowfield more like a stem

--- lova1.js
+++ lova1.js
@@ -1,13 +1,11 @@
 //The following lines of code is inspired by flow field art by Tyler Hobbs, https://www.tylerxhobbs.com/words/flow-fields
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  background(240);
+  background(225, 235, 245);
   noFill();
   strokeWeight(1.2);
 
-  let stepSize = 20;
-  let lineLength = 150;
-
+  let lineLength = 300; // gör linjerna längre
   let palette = [
     color(215, 70, 110, 50),
     color(90, 20, 200, 50),
@@ -15,29 +13,29 @@ function setup() {
     color(20, 120, 150, 50),
   ];
 
-  for (let x = 0; x < width; x += stepSize) {
-    for (let y = 0; y < height; y += stepSize) {
-      stroke(palette[int(random(palette.length))]);
+  // Rita t.ex. 100 linjer från nedre mitten
+  for (let j = 0; j < 100; j++) {
+    stroke(palette[int(random(palette.length))]);
 
-      let px = x;
-      let py = y;
+    let px = width / 2;
+    let py = height;
 
-      beginShape();
-      for (let i = 0; i < lineLength; i++) {
-        vertex(px, py);
+    beginShape();
+    for (let i = 0; i < lineLength; i++) {
+      vertex(px, py);
 
-        // Perlin noise flow field
-        let n = noise(px * 0.002, py * 0.002); 
-        let angle = TAU * n; 
+      // Perlin noise flow field
+      let n = noise(px * 0.002, py * 0.002, j * 0.1); // tredje dimension för variation
+      let angle = TAU * n;
 
-        px += cos(angle) * 2; 
-        py += sin(angle) * 2;
+      px += cos(angle) * 2;
+      py += sin(angle) * 2;
 
-        if (px < 0 || px > width || py < 0 || py > height) break;
-      }
-      endShape();
+      if (px < 0 || px > width || py < 0 || py > height) break;
     }
+    endShape();
   }
 
   noLoop();
 }
+
