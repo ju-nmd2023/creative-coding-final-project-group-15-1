5c275808806ec40004fbbcac02d6f6048660820a
lov-kon <149784438+lov-kon@users.noreply.github.com>
2025-10-06 14:52:29 +0200

    Update lova1.js

--- lova1.js
+++ lova1.js
@@ -5,15 +5,10 @@ function setup() {
   noFill();
   strokeWeight(1.2);
 
-  let stepSize = 20;
+  let stepSize = 10;
   let lineLength = 150;
 
-  let palette = [
-    color(215, 70, 110, 50),
-    color(90, 20, 200, 50),
-    color(255, 150, 50, 50),
-    color(20, 120, 150, 50),
-  ];
+  let palette = [color(215, 70, 110, 50)];
 
   for (let x = 0; x < width; x += stepSize) {
     for (let y = 0; y < height; y += stepSize) {
@@ -27,10 +22,10 @@ function setup() {
         vertex(px, py);
 
         // Perlin noise flow field
-        let n = noise(px * 0.002, py * 0.002); 
-        let angle = TAU * n; 
+        let n = noise(px * 0.002, py * 0.002);
+        let angle = TAU * n;
 
-        px += cos(angle) * 2; 
+        px += cos(angle) * 2;
         py += sin(angle) * 2;
 
         if (px < 0 || px > width || py < 0 || py > height) break;
